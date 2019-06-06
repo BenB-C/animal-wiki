ae3d47deb193f6f97e87b90da6aa4ef2a41e3ba2
Ben Bennett-Cauchon and Garland Lai <garland@>
2019-06-06 10:30:45 -0700

    add walrus pictures and click functionality to toggle info

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  // $("img").click(function() {
+  //   // With toggle
+  //   $(this).parent().children(".info").fadeToggle();
+  //
+  //   // With fadeIn/Out
+  //   // if ($(this).parent().children(".info").is(":visible")) {
+  //   //   $(this).parent().children(".info").fadeOut();
+  //   // } else {
+  //   //   $(this).parent().children(".info").fadeIn();
+  //   // }
+  // });
+
+  $(".col-md-4").click(function() {
+    $(this).children(".info").slideToggle();
+  });
+});
